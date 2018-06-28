12d2e403f36362d0ae211e89c1b9a1704b638936
Joey Kathan <kathan.joey@gmail.com>
2018-06-27 22:08:43 -0400

    first api call working

--- index.js
+++ index.js
@@ -1,45 +1,80 @@
+const flickrLicense = [
+{uiDesc: "No Domain", apiVal: 1}, 
+{uiDesc: "", apiVal: 2}, 
+{uiDesc: "", apiVal: 3}, 
+{uiDesc: "", apiVal: 4}, 
+{uiDesc: "", apiVal: 5}, 
+{uiDesc: "", apiVal: 6}, 
+{uiDesc: "", apiVal: 7}, 
+{uiDesc: "", apiVal: 8}, 
+{uiDesc: "", apiVal: 9}, 
+]
+//write array for other apis where uiDesc is the same, apiVal is different
+
+function mapLicenseType(uiLicenseType, apiName) {
+    let mapToUse = (apiName === 'flickr') ? flickrLicense: null;
+    if (mapToUse === null) {
+        return '';
+    }
+    for (let i=0; i < mapToUse.length; i++) {
+        if (uiLicenseType === mapToUse[i].uiDesc) {
+            return mapToUse[i].apiVal
+        }
+    }
+    return '';
+}
+
 function submitAction() {
-    $('#search-term').submit(function (event) {
+    $('#searchTerm').submit(function (event) {
         event.preventDefault();
         var queryTarget = $(event.currentTarget).find('#query');
         var searchTerm = queryTarget.val();
-        getRequest(searchTerm);
-        //console.log(params.maxResults)
+        var licenseQuery = $(event.currentTarget).find('.license');
+        var licenseType = licenseQuery.val()
+        getRequest(searchTerm, licenseType);
+        //console.log(searchTerm)
     });
 };
-
-function getRequest(searchTerm) {
-    url = 'https://www.googleapis.com/youtube/v3/search';
+//do next three functions for each api
+function getRequest(searchTerm, licenseType) {
+    url = 'https://api.flickr.com/services/rest/';
     const params = {
-        part: 'snippet',
-        key: 'AIzaSyBF5iNtaIhV7dxpUra_Dab06RhnT7rakZQ',
-        q: searchTerm,
-        maxResults: 6,        
+        method: 'flickr.photos.search',
+        api_key: '65d8a1e8c35c504a238f9c7bd46b425c',
+        tags: searchTerm,
+        tagmode: 'all',
+        format: 'json',
+        nojsoncallback: 1,
+        per_page: 6,
+        //Work through owner name
+        license: mapLicenseType(licenseType, 'flickr'),
     };
+    //console.log(params);
   
     $.getJSON(url, params, function (response) {
-        console.log(response.items);
+        console.log(response);
         //showResults(searchTerm);
-         const results = response.items.map((item, response) => showResults(item));
-         $('#search-results').html(results)
-         const totalResults = displayResults(response.items.length);
-         $('.resultCount').html(totalResults)
-         });
+         const results = response.photos.photo.map((item, response) => buildThumbnailUrl(item));
+         $('#flickrResults').html(results)
+         buildThumbnailUrl(response)
+});
+}
+function buildThumbnailUrl(photo) {
+      const thumbnail = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server +
+      '/' + photo.id + '_' + photo.secret + '_q.jpg';
+      console.log(thumbnail);
+      return `<div>
+     <a href="${thumbnail}" tabindex><img src ="${thumbnail}" alt = "${photo.title}"></a>
+    <div>    
+    `;
 }
 
-function showResults (item) {
-    let clickable = 'https://www.youtube.com/watch?v='+`${item.id.videoId}`;
-    return `
-     <div>
-     <a href="${clickable}" tabindex><img src ="${item.snippet.thumbnails.medium.url}" alt = "${item.snippet.title}"></a>
-     <div>    
-    `;
-   }
 
-function displayResults (numCount) {
+
+/*function displayResults (numCount) {
    return `
     <h3> ${numCount} videos displayed on page</h3>
     `;
-}
+}*/
 
 $(submitAction)
